34d7bd70d273b9a3fc7da7c1c805ebf4b63e20b7
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-10-10 09:59:18 -0300

    add new transaction modal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './Header.style';
-
+import { NewTransactionModal } from '../NewTransactionModal/NewTransactionModal';
 import logoImg from '../../assets/Logo.svg';
+
+import * as Dialog from '@radix-ui/react-dialog';
+
 
 export function Header(){
 
@@ -9,7 +12,12 @@ export function Header(){
       <HeaderContent>
         <img src={logoImg} alt="" />
         
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
       
     </HeaderContainer>
